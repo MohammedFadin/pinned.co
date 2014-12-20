343bc03a94c67962223b346b2f28060929c96712
Mohammed S Fadin <imohammad@me.com>
2014-12-20 17:16:05 +0300

    Add tabs channels

--- public/js/pinned.co.js
+++ public/js/pinned.co.js
@@ -10,15 +10,17 @@ jQuery,console, $
 */
 /*jslint todo: true */
 
-var socket, sessions, messagesList, message,
+var socket, sessions, message,
     messageRow, scrollItDown, outputMessage,
     clearUserInput;
 
 socket = io('/pinned-ns');
 sessions = {};
-messagesList = document.getElementById('messagesList');
+//messagesList = document.getElementById('messagesList');
 message = document.getElementById('message');
 messageRow = [];
+var channelTabs = document.getElementsByClassName('channels tabs');
+
 scrollItDown = function() {
     $('.scrollable-div').stop().animate({
         scrollTop: $(".scrollable-div")[0].scrollHeight
@@ -30,11 +32,13 @@ outputMessage = function(data) {
     messagerow.setAttribute('class', 'media');
     messagerow.setAttribute('id', 'message');
     messagerow.innerHTML = templateMessage;
-
     return messagerow;
 };
+OutputCurrentOnlineUsers = function(data) {
+    $('#totalOnline').html(data.totalUsers); // Then add for each channel
+}
 // Prevent XSS
-clearUserInput = function (input) {
+clearUserInput = function(input) {
     return $('<div/>').text(input).html();
 };
 
@@ -48,8 +52,6 @@ $(document).ready(function() {
 });
 
 $('#nicknameForm').submit(function() {
-    console.log('Form Submitted');
-
     var nickname = $('#nickname').val();
 
     if (!$('#nickname').length) {
@@ -61,27 +63,39 @@ $('#nicknameForm').submit(function() {
     socket.emit('chat login', {
         nickname: nickname,
     });
-
     socket.emit('chat subscribe', {
         nickname: nickname,
         channel: 'general'
     });
-
     socket.emit('chat message', {
         context: nickname + ' has joined the general channel!',
         nickname: nickname,
     }, function(ackData) {
         console.log(ackData);
+        var messagesList = document.getElementById('messagesList-general')
         messagesList.appendChild(outputMessage({
             context: nickname + ' has joined the general channel!',
             nickname: nickname,
         }));
     });
-
     return false;
 });
 
+$('#messageValue').on('input', function () {
+    socket.emit('user typing');
+    console.log('input event working')
+})
+$(window).keydown(function (event) {
+    if (event.which === 13){
+        socket.emit('user stopped typing');
+    }
+})
 $('#chatSendBtn').click(function() {
+    // Here we check which channel tab is active
+    var currChannel = $('.tab-pane.active').attr('id');
+
+    var messagesList = document.getElementById('messagesList-'+currChannel);
+
     messagesList.appendChild(outputMessage({
         context: $('#messageValue').val(),
         nickname: $('#nickname').val()
@@ -90,21 +104,15 @@ $('#chatSendBtn').click(function() {
     socket.emit('chat message', {
         context: $('#messageValue').val(),
         nickname: $('#nickname').val(),
+        channel: currChannel,
     }, function(ackData) {
-        console.log(ackData);
-        // if connection intrrupted
-        // fire a popup
     });
     $('#messageValue').val('');
     $('#messageValue').focus();
     scrollItDown();
+    socket.emit('user stopped typing');
     console.log('Message Sent!');
     return false;
-});
-
-socket.on('chat message', function(data) {
-    messagesList.appendChild(outputMessage(data));
-    scrollItDown();
 });
 
 $('#sendPrivate').click(function() {
@@ -115,11 +123,71 @@ $('#sendPrivate').click(function() {
     });
 });
 
-socket.on('chat private', function(msg) {
+$(document).on("click", ".channels.card", function (e) {
+    var btn = $(this).attr("data-channel");
+    var tabsMainDiv = document.getElementById('channelsTabsContent');
+
+    if ($('#'+btn).length === 0){
+        var atab = document.createElement('li');
+        $('.nav.nav-tabs.channels.tabs li').removeClass('active');
+            atab.setAttribute('id', btn+'-tab');
+            atab.setAttribute('class', 'active');
+        var tabStyle = '<a href="#'+btn+'" data-toggle="tab" aria-expanded="true">'+btn+'</a>';
+        var tabHTMLContent = '<div class="panel-body scrollable-div"><ul id="messagesList-'+btn+'" class="media-list"></ul></div>'
+        var tabNodeContent = document.createElement('div');
+            tabNodeContent.setAttribute('class', 'tab-pane fade');
+            tabNodeContent.setAttribute('id', btn);
+        tabNodeContent.innerHTML = tabHTMLContent;
+        atab.innerHTML = tabStyle;
+
+        channelTabs[0].appendChild(atab);
+        tabsMainDiv.appendChild(tabNodeContent);
+        $('.tab-content .tab-pane').removeClass('active');
+        $('#'+btn).addClass('active');
+    }else{
+        $('.nav.nav-tabs.channels.tabs li').removeClass('active');
+        $('#'+btn+'-tab').addClass('active');
+        $('.tab-content .tab-pane').removeClass('active');
+        $('#'+btn).addClass('active');
+    }
+})
+socket.on('chat message', function(data) {
+    messagesList.appendChild(outputMessage(data));
+    scrollItDown();
+});
+socket.on('chat history counter', function (data) {
+    $('#totalJoined').html(data.totalJoined);
+});
+socket.on('private message', function(msg) {
     console.log(msg.from + ': ' + msg.context + ' Time ' + msg.time);
 });
-socket.on('disconnect', function() {
-    console.log("disconnected");
-    //socket.disconnect();
-    //TODO: Insert notification here
+socket.on('user joined', function(data) {
+    OutputCurrentOnlineUsers(data); // update total users
 });
+socket.on('user left', function(data) {
+    OutputCurrentOnlineUsers(data);
+    messagesList.appendChild(outputMessage({
+        context: data.nickname + ' has left the chat',
+        nickname: 'Chat Bot'
+    }));
+    scrollItDown();
+});
+socket.on('user typing', function(data) {
+    $('#typing').fadeIn();
+    scrollItDown();
+});
+socket.on('user stopped typing', function (data) {
+    $('#typing').fadeOut();
+    scrollItDown();
+})
+socket.on('disconnect', function() {
+    console.log("Multiple name sessions detected");
+    // alert('Kicked Current Session!');
+});
+
+/*
+    TODO: 'refactor appending messages',
+          'rename listeners',
+          'cache user information'
+
+ */
--- server-socket.js
+++ server-socket.js
@@ -2,6 +2,7 @@
 module.exports.listen = function(server) {
   var io = require('socket.io')(server);
   var pinnedNS = io.of('/pinned-ns');
+  var pinnedBOTName = 'Chat Bot';
   var connectedUsers = {}; // Track joined/left users sockets
   var userSessions = {}; // Track user sockets sessions
   var channels = [
@@ -17,6 +18,8 @@ module.exports.listen = function(server) {
       return channel;
     }
   }
+
+  var chatHistoryCounter = 0;
 
   pinnedNS.on('connection', function(client) {
     console.log('A socket connection has established');
@@ -39,16 +42,29 @@ module.exports.listen = function(server) {
       for (var prop in userSessions[msg.nickname]) {
         if (userSessions[msg.nickname].hasOwnProperty('socket')) {
           console.log('User established connection twice, kick him!');
+          // userSessions[msg.nickname].socket.broadcast.emit('chat message', {
+          //   context: msg.nickname + ' has rejoined the channel!',
+          //   nickname: pinnedBOTName
+          // });
           userSessions[msg.nickname].socket.disconnect();
           delete userSessions[msg.nickname];
         }
       }
       userSessions[msg.nickname] = {};
       userSessions[msg.nickname].socket = client;
-      // Store the user channel
+      client.nickname = msg.nickname;
+      client.channel = joinTo(msg.channel);
       connectedUsers[msg.nickname].channel = joinTo(msg.channel);
       client.join(joinTo(msg.channel)); // Welcome Message
+      pinnedNS.emit('chat history counter', {totalJoined: ++chatHistoryCounter})
+      pinnedNS.emit('user joined', {
+        totalUsers: Object.keys(connectedUsers).length
+      });
       console.log('client joined this ' + connectedUsers[msg.nickname].channel);
+    });
+
+    client.on('chat subscribe channel', function (msg) {
+
     });
 
     /*
@@ -72,29 +88,40 @@ module.exports.listen = function(server) {
       On user sends a private message to
       another user in same channel
      */
-    client.on('chat private', function(msg) {
-      connectedUsers[msg.to].socket.emit('chat private', {
+    client.on('private message', function(msg) {
+      connectedUsers[msg.to].socket.emit('private message', {
         context: msg.context,
         from: connectedUsers[msg.from].nickname,
         to: connectedUsers[msg.to].nickname,
-        time: new Date().getTime()
+        time: new Date().getTime() // Change to getHours
       });
     });
 
     /*
       On user leavs a channel/room
+      but still connected to namespace
      */
     client.on('chat leave', function() {
       client.leave();
       console.log('A user left from channel/room');
     });
-
+    client.on('user typing', function () {
+        client.broadcast.to(client.channel).emit('user typing')
+    })
+    client.on('user stopped typing', function () {
+        client.broadcast.to(client.channel).emit('user stopped typing')
+    })
     /*
       On user connections disconnects
      */
     client.on('disconnect', function() {
+      console.log(client.nickname + ' disconnected from Pinned.CO');
+      client.broadcast.emit('user left', {
+        totalUsers: Object.keys(connectedUsers).length - 1,
+        nickname: client.nickname
+      });
+      delete connectedUsers[client.nickname];
       client.disconnect();
-      console.log('A user disconnected from Pinned.CO');
     });
   });
 }; // end
